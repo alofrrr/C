fb1be3e07ae28265433934f49794d26d4a9dee47
Ana Laura <analauraoferreira1@gmail.com>
2021-12-09 20:38:42 -0300

    Update cartelaBingo.c

--- cartelaBingo.c
+++ cartelaBingo.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-int main()
-{
-setlocale(LC_ALL, "Portuguese");
-srand(time(NULL));
-int m[5][5], i, j, vetor[25], aux;
-printf("Sua cartela: \n\n");
-for (i = 0; i < 25; i++) {
-do {
-vetor[i] = rand() % 100;
-aux = 0;
-for (j = 0; j < i; j++)
-if (vetor[i] == vetor[j])
-aux = 1;
-} while (aux == 1);
-}
-aux = 0;
-for (i=0; i<5; i++){
-for (j=0; j<5; j++){
-m[i][j] = vetor[aux];
-aux++;
-}
-}
-for(i=0; i<5; i++){
-for(j=0; j<5; j++){
-printf("[%d] ", m[i][j]);
-}
-printf("\n");
-}
-printf("\nBoa sorte!\n");
-return 0;
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	srand(time(NULL));
+	int m[5][5], i, j, vetor[25], aux;
+	printf("Sua cartela: \n\n");
+	for (i = 0; i < 25; i++) {
+		do {
+			vetor[i] = rand() % 100;
+			aux = 0;
+			for (j = 0; j < i; j++)
+				if (vetor[i] == vetor[j])
+					aux = 1;
+		} while (aux == 1);
+	}
+	aux = 0;
+	for (i=0; i<5; i++) {
+		for (j=0; j<5; j++) {
+			m[i][j] = vetor[aux];
+			aux++;
+		}
+	}
+	for(i=0; i<5; i++) {
+		for(j=0; j<5; j++) {
+			printf("[%d] ", m[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\nBoa sorte!\n");
+	return 0;
 }
